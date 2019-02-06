a4871e5dad21ca1d8f6a5881ba5ce2a8ab648185
realStranger <46593031+realStranger@users.noreply.github.com>
2019-02-06 11:49:55 +0300

    Update 4.js

--- js/4.js
+++ js/4.js
@@ -11,39 +11,4 @@ $(document).ready(function(){
   });
 });
 
-(function() {
-  var delay = false;
-
-  $(document).on('mousewheel DOMMouseScroll', function(event) {
-    event.preventDefault();
-    if(delay) return;
-
-    delay = true;
-    setTimeout(function(){delay = false},500)
-
-    var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
-
-    var a= document.getElementsByClassName('nothing');
-    if(wd < 0) {
-      for(var i = 0 ; i < a.length ; i++) {
-        var t = a[i].getClientRects()[0].top;
-        if(t >= 40) break;
-      }
-    }
-    else {
-      for(var i = a.length-1 ; i >= 0 ; i--) {
-        var t = a[i].getClientRects()[0].top;
-        if(t < -20) break;
-      }
-    }
-    
-    if(i >= 0 && i < a.length) {
-      $('html,body').animate({
-        scrollTop: a[i].offsetTop
-      });
-    }
-  });
-})();
-
-
 
